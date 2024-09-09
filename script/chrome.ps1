d9be09c42b64ebbae76b60455d3c847d65b4c93e
ronierisonmaciel <ronierison.maciel@gmail.com>
2024-09-09 11:55:32 -0300

    :fire: Refactor PowerShell script to monitor and limit memory usage of Chrome processes

--- script/chrome.ps1
+++ script/chrome.ps1
@@ -1,4 +1,9 @@
 for ($i=0; $i -le 10; $i++) {
-    Get-Process| where {($_.ProcessName -like "chrome") -and ($_.ws -gt 300000000)}
+    # Passo 1: Obtém todos os processos em execução no sistema com o comando Get-Process.
+    # Passo 2: Filtra os processos, mantendo apenas aqueles cujo nome contém "chrome" 
+    # e que estejam utilizando mais de 300 MB de memória (WorkingSet).
+    Get-Process | where {($_.ProcessName -like "chrome") -and ($_.ws -gt 300000000)}
+
+    # Passo 3: Faz uma pausa de 3 segundos antes de executar a próxima iteração do loop.
     Start-Sleep 3
 }
